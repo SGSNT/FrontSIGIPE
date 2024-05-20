f32fa54bfee60fde82699931c5944ec5c5447d45
Saler C. Lourenço <saler.cez@gmail.com>
2024-05-20 20:05:47 -0300

    cria a estrutura em frame

--- src/app/components/layout/estrutura/frame/frame.component.ts
+++ src/app/components/layout/estrutura/frame/frame.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SidenavComponent } from '../sidenav/sidenav.component';
+import { MainComponent } from '../main/main.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-frame',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SidenavComponent, MainComponent, FooterComponent],
   templateUrl: './frame.component.html',
   styleUrl: './frame.component.scss'
 })
