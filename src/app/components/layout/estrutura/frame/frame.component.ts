30ec6d2f42a67ac48f68a89ca3bea128e67ffda5
Saler C. Lourenço <saler.cez@gmail.com>
2024-05-20 20:35:41 -0300

    remove os footer

--- src/app/components/layout/estrutura/frame/frame.component.ts
+++ src/app/components/layout/estrutura/frame/frame.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidenavComponent } from '../sidenav/sidenav.component';
 import { MainComponent } from '../main/main.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-frame',
   standalone: true,
-  imports: [HeaderComponent, SidenavComponent, MainComponent, FooterComponent],
+  imports: [HeaderComponent, SidenavComponent, MainComponent],
   templateUrl: './frame.component.html',
   styleUrl: './frame.component.scss'
 })
