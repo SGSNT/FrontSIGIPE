efa56a16689ce16fcdc6c4c8ca2af49d6de43b54
samuel Dos Santos Gonçalves da Silva <samuelsantos20021@gmail.com>
2024-05-19 22:12:33 -0300

    Tela de login adicionada, falta dar pequenos ajustes

---
+++ src/app/components/layout/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MdbFormsModule,FormsModule, MdbDropdownModule,],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  login!: string;
+  senha!: string;
+
+  router = inject(Router);
+
+  logar(){
+    
+    if(this.login == 'admin' && this.senha == 'admin'){
+
+      this.router.navigate(['/login']);
+
+    }else{
+      alert('Login ou senha inválidos!');
+    }
+  }
+
+}
