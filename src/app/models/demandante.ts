673a0a699f53e6f2f3afb4a5f350ffa2bfd78ecd
Saler C. Lourenço <saler.cez@gmail.com>
2024-05-22 11:17:39 -0300

    Ajusta realacionamento e cria construtores nas classes. Falta classes de herança

--- src/app/models/demandante.ts
+++ src/app/models/demandante.ts
@@ -1,7 +1,27 @@
+import { Demanda } from "./demanda";
+import { Instituicao } from "./instituicao";
+
 export class Demandante {
     id!: number;
     nome!: string;
     email!: string;
     telefone!: string;
+    instituicao!: Instituicao;
+    demandas: Demanda[] = [];
 
+    constructor(
+        id: number,
+        nome: string,
+        email: string,
+        telefone: string,
+        instituicao: Instituicao,
+        demandas: Demanda[]
+    ) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.instituicao = instituicao;
+        this.demandas = demandas;
+    }
 }
--- src/app/models/statusdemanda.ts
+++ src/app/models/statusdemanda.ts
@@ -1,2 +1,13 @@
+import { Demanda } from "./demanda";
+
 export class Statusdemanda {
+    id!: number;
+    nome!: string;
+    demandas: Demanda[] = [];
+
+    constructor(id: number, nome: string, demandas: Demanda[]){
+        this.demandas = demandas;
+        this.id = id;
+        this.nome = nome;
+    }
 }
