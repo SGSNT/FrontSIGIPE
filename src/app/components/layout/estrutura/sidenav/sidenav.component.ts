e1469ab33e7091c385a5a7239015651b07af7ffd
Saler C. Lourenço <saler.cez@gmail.com>
2024-05-24 01:43:22 -0300

    ajusta side nav

--- src/app/components/layout/estrutura/sidenav/sidenav.component.ts
+++ src/app/components/layout/estrutura/sidenav/sidenav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
