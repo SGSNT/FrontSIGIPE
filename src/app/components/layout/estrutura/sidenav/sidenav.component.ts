423f24373122bad44044112a191c2ea0dc548594
Saler C. Lourenço <saler.cez@gmail.com>
2024-06-11 19:08:42 -0300

    configura navbar para apresentar diferentes botões nos 4 conjuntos de rota

--- src/app/components/layout/estrutura/sidenav/sidenav.component.ts
+++ src/app/components/layout/estrutura/sidenav/sidenav.component.ts
@@ -1,3 +1,4 @@
+import { withNoXsrfProtection } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
@@ -10,4 +11,22 @@ import { RouterLink } from '@angular/router';
 })
 export class SidenavComponent {
 
+  constructor(){
+    this.verificaUrl();
+  }
+  verificaUrl(): number{
+    let url = window.location.pathname;
+    if(url === '/aluno/demandas-disponiveis'){
+      console.log(1);
+      return 1;
+    } else if(url === '/professor/demandas-disponiveis'){
+      console.log(2);
+      return 2;
+    } else if(url === '/coordenacao-curso/demandas-disponiveis'){
+      console.log(3);
+      return 3;
+    }
+    console.log(4);
+    return 4;
+  }
 }
